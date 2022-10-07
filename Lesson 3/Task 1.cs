0d38e7279c6bcdfa1bf7fc0e7e6094f3ed680e8c
Vladislav-Poberezhets <vladpoberezhets1@gmail.com>
2022-10-07 13:56:28 +0300

    Add project files.

---
+++ Lesson 3/Task 1.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введіть перше число:");
+int X = int.Parse(Console.ReadLine());
+Console.WriteLine("Введіть друге число:");
+int Y = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+if (X > Y)
+{
+    for (int i = Y; i <= X; i++)
+    {
+        sum += i;
+    }
+
+}
+else if (Y > X)
+{
+    for (int j = X; j <= Y; j++)
+    {
+        sum += j;
+    }
+
+}
+else
+{
+    sum = X;
+}
+
+
+Console.WriteLine($"Сума чисeл: {sum}");
---
+++ Task 2/Task 2.cs
@@ -0,0 +1,11 @@
+﻿try
+{
+    Console.WriteLine("Введіть перше число:");
+    int X = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введіть друге число:");
+    int Y = int.Parse(Console.ReadLine());
+}
+catch 
+{
+    Console.WriteLine("Invalid input");
+}
